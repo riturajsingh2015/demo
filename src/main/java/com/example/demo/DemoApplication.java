f9c36520067c827757813ee169f7767272b125e5
riturajsingh2015 <riturajsingh2015@gmail.com>
2022-06-25 13:54:25 +0200

    Test and Push commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+	//This s the entry point of the Application
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
