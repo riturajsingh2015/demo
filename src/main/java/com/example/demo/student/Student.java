4fa7539e167fafbe194399c9959fdffe10166a51
riturajsingh2015 <riturajsingh2015@gmail.com>
2022-06-25 15:32:31 +0200

    Data Access interface created

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -2,7 +2,28 @@ package com.example.demo.student;
 
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+
+@Entity //This one is for hibernate
+@Table // This one is table in our ta
 public class Student {
+
+    @Id
+    @SequenceGenerator(
+        name="student_sequence",
+        sequenceName = "student_sequence",
+        allocationSize = 1
+    )
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String email;
@@ -19,7 +40,7 @@ public class Student {
         this.dob = dob;
         this.age = age;
     }
-
+ 
     public Student(String name, String email, LocalDate dob, Integer age) {
         this.name = name;
         this.email = email;
